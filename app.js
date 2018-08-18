4e84cd5a0b47193fc27bd89e518cdf1c4bcee7dc
Gh0bo <42476870+Gh0bo@users.noreply.github.com>
2018-08-18 12:35:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1867,7 +1867,7 @@ client.on('message', message => {
         });
     }
 
-    if (command === "ec-rush-mlg") {
+        if (command === "ec-rush-mlg") {
         const player = args.join(' ');
         message.delete();
 
@@ -1904,7 +1904,7 @@ client.on('message', message => {
                     },
                     {
                         name: "Winrate en rush:",
-                        value: parseInt(obj.stats.rush.stat_win / (obj.stats.rush.stat_played_game) * 100) + "%"
+                        value: parseInt(obj.stats.rush.stat_win / obj.stats.rush.stat_played_game * 100) + "%"
                     },
                     {
                         name: "Nombre total de lits cassés en rush",
@@ -1928,7 +1928,7 @@ client.on('message', message => {
                     },
                     {
                         name: "Winrate en mlg:",
-                        value: parseInt(obj.stats.rush_mlg.stat_win / (obj.stats.rush_mlg.stat_played_game) * 100) + "%"
+                        value: parseInt(obj.stats.rush.stat_rush_mlg_win / (obj.stats.rush.stat_rush_mlg_played) * 100) + "%"
                     },
                     {
                         name: "Nombre total de points marqués en mlg",
